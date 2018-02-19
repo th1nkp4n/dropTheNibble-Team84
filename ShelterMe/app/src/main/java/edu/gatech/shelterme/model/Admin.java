aa833d40836f884393e9ff17fcf329b7d9162f28
mehaKumar <mehaonweb@gmail.com>
2018-02-18 19:15:53 -0500

    intent extra

--- ShelterMe/app/src/main/java/edu/gatech/shelterme/model/Admin.java
+++ ShelterMe/app/src/main/java/edu/gatech/shelterme/model/Admin.java
@@ -1,6 +1,5 @@
 package edu.gatech.shelterme.model;
 
-import edu.gatech.shelterme.User;
 
 /**
  * Created by ttsubota3 on 2/18/18.
@@ -11,6 +10,7 @@ public class Admin extends User {
     public Admin(String name) {
         this.name = name;
     }
+    public Admin() {}
     String getName() {
         return name;
     }
