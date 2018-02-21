adf5b4bdd691957299138a3037356987e228e70c
mehaKumar <mehaonweb@gmail.com>
2018-02-20 23:42:25 -0500

    fix username/email discrepancy

--- ShelterMe/app/src/main/java/edu/gatech/shelterme/controllers/BadLoginAlertDialogFragment.java
+++ ShelterMe/app/src/main/java/edu/gatech/shelterme/controllers/BadLoginAlertDialogFragment.java
@@ -15,7 +15,7 @@ public class BadLoginAlertDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("You have entered an incorrect Username or Password.")
+        builder.setMessage("You have entered an incorrect Email or Password.")
                 .setNegativeButton("Okay", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                     }
