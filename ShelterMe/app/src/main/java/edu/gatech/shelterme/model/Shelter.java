88f719e89449e921204f71d49858536e93ca5f7c
mehaKumar <mehaonweb@gmail.com>
2018-02-25 16:06:31 -0500

    add shelter class

---
+++ ShelterMe/app/src/main/java/edu/gatech/shelterme/model/Shelter.java
@@ -0,0 +1,91 @@
+package edu.gatech.shelterme.model;
+
+/**
+ * Created by Meha on 2/25/2018.
+ */
+
+public class Shelter {
+    private String name;
+    private int capacity;
+    private String restriction;
+    private double longitude;
+    private double latitude;
+    private String address;
+    private String specialNotes;
+    private String phone;
+
+    public Shelter(String name, int capacity, String restriction, double longitude, double latitude, String address, String specialNotes, String phone) {
+        this.name = name;
+        this.capacity = capacity;
+        this.restriction = restriction;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.address = address;
+        this.specialNotes = specialNotes;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getRestriction() {
+        return restriction;
+    }
+
+    public void setRestriction(String restriction) {
+        this.restriction = restriction;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSpecialNotes() {
+        return specialNotes;
+    }
+
+    public void setSpecialNotes(String specialNotes) {
+        this.specialNotes = specialNotes;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
