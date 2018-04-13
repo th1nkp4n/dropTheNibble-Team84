395d1df0a9ba1482dc69a079907b584c3d11b28a
Tsubota, Tamara <ttsubota3@lawn-128-61-74-207.lawn.gatech.edu>
2018-04-13 14:29:59 -0400

    social security number test passes

---
+++ ShelterMe/app/src/test/java/edu/gatech/shelterme/model/WorkerTest.java
@@ -0,0 +1,45 @@
+package edu.gatech.shelterme.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ttsubota3 on 4/13/18.
+ */
+public class WorkerTest {
+    private WorkerSocial user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new WorkerSocial();
+    }
+
+    /**
+     * Tests no social security number
+     */
+    @Test
+    public void testNoNumber() {
+        String message = user.correctSocial("");
+        assertEquals("number is too short", message);
+    }
+
+    /**
+     * Tests a too long social security number
+     */
+    @Test
+    public void testTooLong() {
+        String message = user.correctSocial("0123456789");
+        assertEquals("number is too long", message);
+    }
+
+    /**
+     * Tests a valid social security number
+     */
+    @Test
+    public void testValidNumber() {
+        String message = user.correctSocial("123456789");
+        assertEquals("valid number", message);
+    }
+}
