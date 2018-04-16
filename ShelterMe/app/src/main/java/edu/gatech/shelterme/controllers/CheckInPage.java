28ed660495c4be622c21b4da1a9ca026165a79b6
Meggles711 <mhuber@gatech.edu>
2018-04-15 22:29:59 -0400

    Added tests for checkIn entry correctness

--- ShelterMe/app/src/main/java/edu/gatech/shelterme/controllers/CheckInPage.java
+++ ShelterMe/app/src/main/java/edu/gatech/shelterme/controllers/CheckInPage.java
@@ -19,6 +19,7 @@ import com.google.firebase.database.ValueEventListener;
 import edu.gatech.shelterme.R;
 import edu.gatech.shelterme.model.Homeless;
 import edu.gatech.shelterme.model.Shelter;
+import edu.gatech.shelterme.model.WorkerSocial;
 
 public class CheckInPage extends AppCompatActivity {
     private DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
@@ -121,21 +122,20 @@ public class CheckInPage extends AppCompatActivity {
                                 if (numFam.getVisibility() == View.VISIBLE) {
                                     int numFV = Integer.valueOf(numFam.getText().toString());
                                     int newVac = shelter.getFamilyVacancies() - numFV;
-                                    if (numFV == 0){
-                                        goodCheckIn = 0;
+                                    WorkerSocial social = new WorkerSocial();
+                                    goodCheckIn = social.goodCheckIn(numFV, newVac);
+                                    if (goodCheckIn == 0){
                                         Log.d("log :", "didn't check in any families");
                                         Homeless homelessPerson = new Homeless();
                                         homelessPerson.setCheckedIn(-1, key);
 
-                                    } else if (newVac < 0) {
-                                        goodCheckIn = -1;
+                                    } else if (goodCheckIn == -1) {
                                         Log.d("log: ","over family cap");
                                         BadFamilyCheckInAlertDialogFragment badCheckIn = new BadFamilyCheckInAlertDialogFragment();
                                         FragmentTransaction ft = getFragmentManager().beginTransaction();
                                         badCheckIn.show(ft, "maybe");
                                         newVac = shelter.getFamilyVacancies() - numFV;
                                     } else {
-                                        goodCheckIn = 1;
                                         Homeless person = new Homeless();
                                         person.setFamilies(numFV, key);
                                         shelter.setFamilyVacancies(newVac, Integer.toString(shelterID));
